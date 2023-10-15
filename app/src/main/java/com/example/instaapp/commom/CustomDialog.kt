be06f2977c3998a82b480a34fb7e1b1d37d51408
Lucas Pereira <lucas.pereira@valemobi.com.br>
2023-10-15 14:56:09 -0300

    Subindo modal de galeria fotos etc

---
+++ app/src/main/java/com/example/instaapp/commom/CustomDialog.kt
@@ -0,0 +1,49 @@
+package com.example.instaapp.commom
+
+import android.app.Dialog
+import android.content.Context
+import android.media.MediaCodec.LinearBlock
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.example.instaapp.R
+
+class CustomDialog(contex: Context): Dialog(contex) {
+
+    private lateinit var dialogLinearLayoout: LinearLayout
+    private lateinit var txtButtons: Array<TextView>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dialog_custom)
+
+        dialogLinearLayoout = findViewById(R.id.dialog_container)
+    }
+
+    fun addButton(vararg texts: Int, listener: View.OnClickListener) {
+        txtButtons = Array(texts.size) {
+            TextView(context)
+        }
+
+        texts.forEachIndexed { index, txtId ->
+            txtButtons[index].id = txtId
+            txtButtons[index].setText(txtId)
+            txtButtons[index].setOnClickListener {
+                listener.onClick(it)
+                dismiss()
+            }
+        }
+    }
+
+    override fun show() {
+        super.show()
+        for (textView in txtButtons) {
+            val layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+            layoutParams.setMargins(30, 50,30, 50)
+
+
+            dialogLinearLayoout.addView(textView, layoutParams)
+        }
+    }
+}
--- app/src/main/java/com/example/instaapp/register/RegisterAddPhotosFragment.kt
+++ app/src/main/java/com/example/instaapp/register/RegisterAddPhotosFragment.kt
@@ -1,11 +1,14 @@
 package com.example.instaapp.register
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.instaapp.R
+import com.example.instaapp.commom.CustomDialog
+
 
 class RegisterAddPhotosFragment: Fragment() {
 
@@ -15,5 +18,24 @@ class RegisterAddPhotosFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_register_add_photos, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val customDialog = CustomDialog(requireContext())
+        customDialog.addButton(R.string.photo, R.string.gallery, R.string.app_name) {
+            when (it.id) {
+                R.string.photo -> {
+                    val intent = Intent("android.media.action.IMAGE_CAPTURE")
+                    startActivity(intent)
+                }
+                R.string.gallery -> {
+
+                }
+            }
+        }
+        customDialog.show()
+
     }
 }
