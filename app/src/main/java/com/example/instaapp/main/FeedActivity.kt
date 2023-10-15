1243b0104faa3008ae2322432714888fe37ea425
Lucas Pereira <lucas.pereira@valemobi.com.br>
2023-10-15 16:43:49 -0300

    Add o bottom navigation de acordo com o insta

---
+++ app/src/main/java/com/example/instaapp/main/FeedActivity.kt
@@ -0,0 +1,18 @@
+package com.example.instaapp.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.instaapp.R
+import com.example.instaapp.databinding.ActivityFeedBinding
+
+class FeedActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFeedBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFeedBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
