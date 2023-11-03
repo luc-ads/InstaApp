24a351c7abcaa0f52f0ec9bba470ed92d0e1af4a
Lucas Pereira <lucas.pereira@valemobi.com.br>
2023-11-03 09:18:39 -0300

    Evolução do fragment de profile

--- app/src/main/java/com/example/instaapp/profille/view/ProfileFragment.kt
+++ app/src/main/java/com/example/instaapp/profille/view/ProfileFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.instaapp.R
+import com.example.instaapp.databinding.ItemProfileGridBinding
 
 class ProfileFragment: Fragment() {
 
@@ -17,5 +20,37 @@ class ProfileFragment: Fragment() {
         return inflater.inflate(R.layout.profile_fragment, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        configRv(view)
+    }
+
+    private fun configRv(view: View) {
+        val rv = view.findViewById<RecyclerView>(R.id.profile_rv)
+        rv.layoutManager = GridLayoutManager(requireContext(), 3)
+        rv.adapter = PostAdapter()
+    }
+
+
+    private class PostAdapter: RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+            val binding = ItemProfileGridBinding.inflate(LayoutInflater.from(parent.context),parent, false)
+
+            return PostViewHolder(binding)
+        }
+
+        override fun getItemCount(): Int = 20
+
+        override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+            holder.bind(R.drawable.ic_insta_add)
+        }
+
+        private class PostViewHolder(var itemBinding:ItemProfileGridBinding): RecyclerView.ViewHolder(itemBinding.root) {
+            fun bind(image: Int) {
+                itemBinding.itemProfileImgGrid.setImageResource(image)
+            }
+        }
+    }
 }
