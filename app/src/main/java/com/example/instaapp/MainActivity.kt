2a1915e11ecd451bc25621f75e6a2835f1fa8b4a
Lucas Pereira <lucas.pereira@valemobi.com.br>
2023-11-15 19:43:10 -0300

    Alterando a cor da status bar color e dos ícones dela

--- app/src/main/java/com/example/instaapp/MainActivity.kt
+++ app/src/main/java/com/example/instaapp/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.example.instaapp
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsetsController
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import com.example.instaapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        window.insetsController?.setSystemBarsAppearance(
+            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+        )
+
+        window.statusBarColor = ContextCompat.getColor(
+            this,
+            R.color.gray
+        )
     }
 }
