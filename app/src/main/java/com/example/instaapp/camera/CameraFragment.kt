b92307aa098d7c41383b394771dd1e8e8a457b44
Lucas Pereira <lucas.pereira@valemobi.com.br>
2023-11-16 21:45:03 -0300

    Add novas features visuais

---
+++ app/src/main/java/com/example/instaapp/camera/CameraFragment.kt
@@ -0,0 +1,27 @@
+package com.example.instaapp.camera
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.instaapp.R
+import com.example.instaapp.databinding.ItemPostListBinding
+import com.example.instaapp.databinding.ItemProfileGridBinding
+
+class CameraFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.galeria_fragment, container, false)
+    }
+
+}
